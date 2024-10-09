68b5f695d9a8a49e16446c4a8bd02a8ce771102c
JCollinsX <nonfungiblejc@gmail.com>
2024-10-10 03:14:33 +0900

    fix: :sparkles: purchase history sync from base sepolia & bsc Testnet

--- src/token-factory.ts
+++ src/token-factory.ts
@@ -1,12 +1,10 @@
-import { Bytes, log } from "@graphprotocol/graph-ts";
+import { BigInt, Bytes, log } from "@graphprotocol/graph-ts";
 import {
   CreatedMemeToken,
   BoughtMemeToken,
-  SoldMemeToken,
-  WithdrawnETH,
-  WithdrawnToken,
+  SoldMemeToken, BoughtCrosschainMemeToken, SoldCrosschainMemeToken,
 } from "../generated/TokenFactory/TokenFactory";
-import { Token } from "../generated/schema";
+import { PurchaseHistory, Token } from "../generated/schema";
 import { BIGINT_ZERO, DEFAULT_DECIMALS } from "./common/constants";
 import {
 StandardToken,
@@ -48,15 +46,75 @@ export function handleCreatedMemeToken(event: CreatedMemeToken): void {
 export function handleBoughtMemeToken(event: BoughtMemeToken): void {
     let token = Token.load(Bytes.fromHexString(event.params.memeTokenAddress.toHex()));
     if (token != null) {
+      const purchaseHistory = new PurchaseHistory(
+        Bytes.fromHexString(event.transaction.hash.toHex()).concatI32(event.logIndex.toI32())
+      )
 
+      purchaseHistory.token = token.id
+      purchaseHistory.account = event.params.user
+      purchaseHistory.amount = event.params.tokenQty
+      purchaseHistory.type = "buy"
+      purchaseHistory.eid = BigInt.fromI32(0)
+      purchaseHistory.hash = event.transaction.hash.toHex()
+      purchaseHistory.blockNumber = event.block.number
+      purchaseHistory.timestamp = event.block.timestamp
+      purchaseHistory.save()
     }
 }
 
 export function handleSoldMemeToken(event: SoldMemeToken): void {
+  let token = Token.load(Bytes.fromHexString(event.params.memeTokenAddress.toHex()));
+  if (token != null) {
+    const purchaseHistory = new PurchaseHistory(
+      Bytes.fromHexString(event.transaction.hash.toHex()).concatI32(event.logIndex.toI32())
+    )
+
+    purchaseHistory.token = token.id
+    purchaseHistory.account = event.params.user
+    purchaseHistory.amount = event.params.tokenQty
+    purchaseHistory.type = "sell"
+    purchaseHistory.eid = BigInt.fromI32(0)
+    purchaseHistory.hash = event.transaction.hash.toHex()
+    purchaseHistory.blockNumber = event.block.number
+    purchaseHistory.timestamp = event.block.timestamp
+    purchaseHistory.save()
+  }
 }
 
-export function handleWithdrawnETH(event: WithdrawnETH): void {
+export function handleBoughtCrosschainMemeToken(event: BoughtCrosschainMemeToken): void {
+  let token = Token.load(Bytes.fromHexString(event.params.memeTokenAddress.toHex()));
+  if (token != null) {
+    const purchaseHistory = new PurchaseHistory(
+      Bytes.fromHexString(event.transaction.hash.toHex()).concatI32(event.logIndex.toI32())
+    )
+
+    purchaseHistory.token = token.id
+    purchaseHistory.account = event.params.user
+    purchaseHistory.amount = event.params.tokenQty
+    purchaseHistory.type = "buy"
+    purchaseHistory.eid = event.params.srcEid
+    purchaseHistory.hash = event.transaction.hash.toHex()
+    purchaseHistory.blockNumber = event.block.number
+    purchaseHistory.timestamp = event.block.timestamp
+    purchaseHistory.save()
+  }
 }
 
-export function handleWithdrawnToken(event: WithdrawnToken): void {
+export function handleSoldCrosschainMemeToken(event: SoldCrosschainMemeToken): void {
+  let token = Token.load(Bytes.fromHexString(event.params.memeTokenAddress.toHex()));
+  if (token != null) {
+    const purchaseHistory = new PurchaseHistory(
+      Bytes.fromHexString(event.transaction.hash.toHex()).concatI32(event.logIndex.toI32())
+    )
+
+    purchaseHistory.token = token.id
+    purchaseHistory.account = event.params.user
+    purchaseHistory.amount = event.params.tokenQty
+    purchaseHistory.type = "sell"
+    purchaseHistory.eid = event.params.srcEid
+    purchaseHistory.hash = event.transaction.hash.toHex()
+    purchaseHistory.blockNumber = event.block.number
+    purchaseHistory.timestamp = event.block.timestamp
+    purchaseHistory.save()
+  }
 }
